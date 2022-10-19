93eb11506c63b43367b84191678c5a964254b30f
JoyEsiedesa <jesiedesa@gmail.com>
2022-10-19 09:47:35 -0700

    7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,8 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(in c);
-int print_sign(int n);
+int _isalpha(int);
+int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
 
