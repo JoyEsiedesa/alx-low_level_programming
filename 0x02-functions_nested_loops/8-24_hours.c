c12e789fec9e8fac52968189dd78b85db527b066
JoyEsiedesa <jesiedesa@gmail.com>
2022-10-19 12:45:31 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,10 @@
 /**
  * jack_bauer - to print every minute of the day of
  * Jack Bauer starting from 00:00 to 23:59.
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
+int jack_bauer(void)
 {
 	int hour, minute;
 
