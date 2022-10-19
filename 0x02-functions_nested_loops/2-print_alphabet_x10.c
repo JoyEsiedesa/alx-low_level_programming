e592413525ce0fac98335c380b16f645a6991c99
JoyEsiedesa <jesiedesa@gmail.com>
2022-10-19 06:00:26 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
@@ -11,6 +11,6 @@ void print_alphabet_x10(void);
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('/n');
+		_putchar('\n');
 	}
 
